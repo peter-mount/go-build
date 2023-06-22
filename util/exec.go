b0d1967644a033e3b7ef159998601831ddd1d08c
peter <peter@retep.org>
2023-06-22 20:39:49 +0100

    Initial commit

---
+++ util/exec.go
@@ -0,0 +1,21 @@
+package util
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func Label(label, f string, a ...any) {
+	fmt.Printf("%-8s ", label)
+	fmt.Printf(f, a...)
+	fmt.Println()
+}
+
+func RunCommand(name string, a ...string) error {
+	cmd := exec.Command(name, a...)
+	cmd.Stdout = os.Stdout
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
